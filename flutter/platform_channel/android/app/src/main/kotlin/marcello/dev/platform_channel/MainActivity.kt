047c81d75a40e85b82687c835bd30c97533c02a9
Marcello Câmara <marcellocamara@id.uff.br>
2021-05-03 17:28:57 -0300

    feat: platform channel android setup

--- flutter/platform_channel/android/app/src/main/kotlin/marcello/dev/platform_channel/MainActivity.kt
+++ flutter/platform_channel/android/app/src/main/kotlin/marcello/dev/platform_channel/MainActivity.kt
@@ -1,6 +1,25 @@
 package marcello.dev.platform_channel
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
-class MainActivity: FlutterActivity() {
+class MainActivity : FlutterActivity() {
+
+    private val channelName = "marcello.dev.platform_channel/native"
+    private val methodName = "version"
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, channelName)
+                .setMethodCallHandler { call, result ->
+                    if (call.method.equals(methodName)) {
+                        result.success(BuildConfig.VERSION_NAME)
+                    } else {
+                        result.notImplemented()
+                    }
+                }
+    }
+
 }
